d24f7cdac76873ff5de884fb8216cfa1ba13e5cc
Admin <Admin@E2ML17005>
2016-09-19 23:39:53 -0400

    2nd commit

--- JavaLearning/src/learning/calculator.java
+++ JavaLearning/src/learning/calculator.java
@@ -19,6 +19,7 @@ public class calculator {
 	public static double divide(String s1, String s2){
 		double d1 = Double.parseDouble(s1);
 		double d2 = Double.parseDouble(s2);
+		System.out.println("dishkau");
 		return d1/d2;
 	}
 }
